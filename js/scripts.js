e2ffea7ab462aba06d338d2d1bc7f4fd62b1d8a0
FranDesarrolla <franciscovargascontacto@gmail.com>
2021-03-28 13:39:55 -0300

    Prueba

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+
+    // boton funcionando //
+
+    let byton = document.getElementById("suma");
+	byton.addEventListener("click", ()=> {
+
+
+            let num1 = document.getElementById('x').value;
+            let num2 = document.getElementById('y').value;
+
+            let x = parseInt(num1);
+            let y = parseInt(num2);
+
+            var sumaConcretada = x + y;
+
+            let resulti = document.getElementById("p-cam");
+            resulti.innerText = sumaConcretada;
+    });
+
+    let buton = document.getElementById("resta");
+	buton.addEventListener("click", ()=> {
+
+
+            let num1 = document.getElementById('x').value;
+            let num2 = document.getElementById('y').value;
+
+            let x = parseInt(num1);
+            let y = parseInt(num2);
+
+            var restaConcretada = x - y;
+
+            let resulti = document.getElementById("p-cam");
+            resulti.innerText = restaConcretada;
+    });
+
+    let beton = document.getElementById("multi");
+	beton.addEventListener("click", ()=> {
+
+
+            let num1 = document.getElementById('x').value;
+            let num2 = document.getElementById('y').value;
+
+            let x = parseInt(num1);
+            let y = parseInt(num2);
+
+            var multiConcretada = x * y;
+
+            let resulti = document.getElementById("p-cam");
+            resulti.innerText = multiConcretada;
+    });
+
+    let biton = document.getElementById("divi");
+	biton.addEventListener("click", ()=> {
+
+
+            let num1 = document.getElementById('x').value;
+            let num2 = document.getElementById('y').value;
+
+            let x = parseInt(num1);
+            let y = parseInt(num2);
+
+            var diviConcretada = x / y;
+
+            let resulti = document.getElementById("p-cam");
+            resulti.innerText = diviConcretada;
+    });
+
+    let bjton = document.getElementById("porcentaje");
+	bjton.addEventListener("click", ()=> {
+
+
+            let num1 = document.getElementById('x').value;
+            let num2 = document.getElementById('y').value;
+
+            let x = parseInt(num1);
+            let y = parseInt(num2);
+
+            var porConcretada = x % y;
+
+            let resulti = document.getElementById("p-cam");
+            resulti.innerText = porConcretada;
+    });
